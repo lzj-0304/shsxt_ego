d57b73a9d6a694249f64af5ec77bfc6c1e3ff106
joy <lzj_0304@163.com>
2019-07-02 10:58:32 +0800

    ego_2.0_商品管理_商品上架_下架_删除

--- ego-manager-web/src/main/java/com/shsxt/ego/manager/service/IManagerItemService.java
+++ ego-manager-web/src/main/java/com/shsxt/ego/manager/service/IManagerItemService.java
@@ -1,9 +1,30 @@
 package com.shsxt.ego.manager.service;
 
+import com.shsxt.ego.common.model.EgoResult;
 import com.shsxt.ego.common.model.PageResult;
 import com.shsxt.ego.rpc.pojo.TbItem;
 import com.shsxt.ego.rpc.query.ItemQuery;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 public interface IManagerItemService {
     public PageResult<TbItem> itemList(ItemQuery itemQuery);
+
+
+    /**
+     * 商品上架
+     * @param ids
+     * @return
+     */
+    public EgoResult reshelf(Long[] ids);
+
+    /**
+     * 商品下架
+     * @param ids
+     * @return
+     */
+    public EgoResult instock(Long[] ids);
+
+
+    EgoResult deleteItemBatch(Long[] ids);
 }
--- ego-rpc/ego-rpc-service-impl/src/main/java/com/shsxt/ego/rpc/service/impl/ItemServiceImpl.java
+++ ego-rpc/ego-rpc-service-impl/src/main/java/com/shsxt/ego/rpc/service/impl/ItemServiceImpl.java
@@ -3,20 +3,32 @@ package com.shsxt.ego.rpc.service.impl;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.shsxt.ego.common.model.EgoResult;
 import com.shsxt.ego.common.model.PageResult;
+import com.shsxt.ego.rpc.mapper.db.dao.TbItemDescMapper;
 import com.shsxt.ego.rpc.mapper.db.dao.TbItemMapper;
+import com.shsxt.ego.rpc.mapper.db.dao.TbItemParamItemMapper;
 import com.shsxt.ego.rpc.pojo.TbItem;
 import com.shsxt.ego.rpc.query.ItemQuery;
 import com.shsxt.ego.rpc.service.IItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ItemServiceImpl implements IItemService {
     @Autowired
     private TbItemMapper itemMapper;
+
+    @Autowired
+    private TbItemDescMapper itemDescMapper;
+
+    @Autowired
+    private TbItemParamItemMapper itemParamItemMapper;
 
     @Override
     public PageResult<TbItem> queryItemsListByParams(ItemQuery itemQuery) {
@@ -29,4 +41,39 @@ public class ItemServiceImpl implements IItemService {
         pageResult.setRows(pageInfo.getList());
         return pageResult;
     }
+
+    @Override
+    public EgoResult updateItemStatus(TbItem item) {
+        itemMapper.updateByPrimaryKeySelective(item);
+        return new EgoResult();
+    }
+
+    @Override
+    public EgoResult updateItemStatusBatch(Long[] ids,int type) {
+        Map<String,Object> param=new HashMap<String,Object>();
+        param.put("ids",ids);
+        param.put("type",type);
+        itemMapper.updateItemStatusBatch(param);
+        return new EgoResult();
+    }
+
+    @Override
+    public EgoResult deleteItemBatch(Long[] ids) {
+        /**
+         * 涉及表
+         *   tb_item
+         *   tb_item_desc
+         *   tb_item_param_item
+         */
+        Map<String,Object> param=new HashMap<String,Object>();
+        param.put("ids",ids);
+        itemMapper.deleteItemBatch(param);
+        // 删除商品描述记录
+        itemDescMapper.deleteItemDescBatch(param);
+        // 删除商品规格记录
+        itemParamItemMapper.deleteItemParamItemBatch(param);
+        return new EgoResult();
+    }
+
+
 }
